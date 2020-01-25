a367b79badbfcfd878e8332365f77d19044a3b54
aatishpr <49292267+aatishpr@users.noreply.github.com>
2020-01-25 11:50:01 -0500

    Quoted Aatish Pradhan

--- molecool/functions.py
+++ molecool/functions.py
@@ -23,9 +23,9 @@ def canvas(with_attribution=True):
         Compiled string including quote and optional attribution
     """
 
-    quote = "The code is but a canvas to our imagination!!!!"
+    quote = "Too cool for this MOLEcool!"
     if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
+        quote += "\n\t- Aatish's humour 101"
     return quote
 
 
