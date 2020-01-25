1f9d5052e679ed8763163f551c44938367c1a251
Aatish Pradhan <aatishpr@buffalo.edu>
2020-01-25 14:21:46 -0500

    Structure code into module and sub packages

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -5,6 +5,13 @@ A python package for analyzing and visualizing xyz file. MolSSI Best Practices W
 
 # Add imports here
 from .functions import *
+from .measure import calculate_angle, calculate_distance
+from .visualize import draw_bond_histogram,draw_molecule
+from .molecule import build_bond_list
+from .atom_data import atom_colors, atomic_weights
+
+import molecool.io
+
 
 # Handle versioneer
 from ._version import get_versions
